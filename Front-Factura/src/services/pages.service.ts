3bcb41ea62cff8ae9b266057fc7c526bca7cd385
Steven Muñoz <darkmixt@gmail.com>
2023-10-27 16:49:53 -0600

    claudio changes

--- Front-Factura/src/services/pages.service.ts
+++ Front-Factura/src/services/pages.service.ts
@@ -2,40 +2,52 @@ import { ViewChild, ElementRef, Injectable } from '@angular/core';
 import { FacturaService } from './factura.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { Factura } from 'src/models/factura';
-import { Subject, Observable } from 'rxjs';
-
+import { Subject, Observable, map } from 'rxjs';
 
 @Injectable()
 export class PagesService {
-  dataSource = new MatTableDataSource<Factura>
+  dataSource = new MatTableDataSource<Factura>();
 
-  constructor(
-    private _facturaService: FacturaService
-  ) {}
+  constructor(private _facturaService: FacturaService) {}
 
-  testPagesService(){
-    console.log('Testing from Service Pages')
+  testPagesService() {
+    console.log('Testing from Service Pages');
   }
 
-  
-  //? Jala la info de las facturas de la BQ y las muestra en la tabla // 
+  // //? Jala la info de las facturas de la BQ y las muestra en la tabla //
+  // getFacturas(): Observable<any> {
+  //   var subject = new Subject<any>();
+
+  //     this._facturaService.listaFacturas().subscribe(
+  //       (response) => {
+
+  //         if (response.facturasInfo) {
+  //           console.log(response.facturasInfo); /* para que se llenen los campos */
+  //           subject.next(response.facturasInfo);
+  //         }
+  //       },
+  //       (error) => {
+  //         console.log(<any>error);
+  //       }
+  //     );
+  //   return subject.asObservable();
+  // }
+
+  //? Jala la info de las facturas de la BQ y las muestra en la tabla //
+
   getFacturas(): Observable<any> {
-    var subject = new Subject<any>();
-
-      this._facturaService.listaFacturas().subscribe(
-        (response) => {
- 
-          if (response.facturasInfo) {
-            console.log(response.facturasInfo); /* para que se llenen los campos */
-            subject.next(response.facturasInfo);
-          }
-        },
-        (error) => {
-          console.log(<any>error);
-        }
-      );
-    return subject.asObservable();
+    return this._facturaService.listaFacturas();
   }
-  
 
+  // (response) => {
+  //
+  //   if (response.facturasInfo) {
+  //     console.log(response.facturasInfo); /* para que se llenen los campos */
+
+  // },
+  // (error:any) => {
+  //   console.log(<any>error);
+  // }
+  // );
+  //return
 }
